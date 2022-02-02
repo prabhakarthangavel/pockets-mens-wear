7cd89408a9c9964b3b1ca8b0c1f2819fa6f8b424
Prabhakar Thangavel <prabhakart.ge@gmail.com>
2022-02-02 19:53:07 +0530

    prod env changes for firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyBOKJ2MkIINjxImlvPrkgvl30ExIEQG-Jg",
+  authDomain: "pockets-mens-wear.firebaseapp.com",
+  projectId: "pockets-mens-wear",
+  storageBucket: "pockets-mens-wear.appspot.com",
+  messagingSenderId: "754576245652",
+  appId: "1:754576245652:web:090e1a8dd7a9b836228581",
+  measurementId: "G-1BPF7PBKB4"
+  }
 };
