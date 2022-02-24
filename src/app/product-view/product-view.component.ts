4c9d30c5eee05ed59f5b3967633f56b75e170298
Prabhakar Thangavel <prabhakart.ge@gmail.com>
2022-02-24 17:03:19 +0530

    product view changes

--- src/app/product-view/product-view.component.ts
+++ src/app/product-view/product-view.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product.model';
 import { Subscription } from 'rxjs';
 import { ProductsService } from '../products/products.service';
+// declare var carousel: any;
 
 @Component({
   selector: 'app-product-view',
@@ -13,9 +14,11 @@ export class ProductViewComponent implements OnInit, AfterContentInit, OnDestroy
   public productId: number;
   public product: Product;
   public subscription: Subscription;
+  public selectedImgUrl: string;
   constructor(private route: ActivatedRoute, private _productService: ProductsService) { }
 
   ngOnInit(): void {
+    // new carousel();
     this.route.queryParams.subscribe(
       param => {
         this.productId = param.productId;
@@ -27,8 +30,32 @@ export class ProductViewComponent implements OnInit, AfterContentInit, OnDestroy
       this.subscription = this._productService.getProductDetail(this.productId).subscribe(
         response => {
           console.log(response)
+          if (response && response.status == 200) {
+            let urls: string[] = [];
+            response.body.imageUrl.split(',').forEach((img: string) => {
+              urls.push('https://firebasestorage.googleapis.com/v0/b/pockets-mens-wear.appspot.com/o/uploads%2F' + img + '?alt=media');
+            })
+            const product = {
+              id: response.body.id,
+              actualPrice: response.body.actualPrice,
+              category: response.body.category,
+              description: response.body.description,
+              discountedPrice: response.body.discountedPrice,
+              name: response.body.name,
+              size: response.body.sizes,
+              imageUrls: urls
+            }
+            this.selectedImgUrl = product.imageUrls[0];
+            this.product = product;
+          }
+
         });
     }
+  }
+
+  imageSelected(imgUrl: string) {
+    console.log(imgUrl)
+    this.selectedImgUrl = imgUrl;
   }
 
   ngOnDestroy() {
